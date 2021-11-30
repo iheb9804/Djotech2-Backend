c0b5fa0c0e97c41ba8262c1667f160ff2df7eb17
IHeb <ihebferjani04@gmail.com>
2021-11-30 21:45:39 +0100

    Major commit

---
+++ backend/routes/products.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const product=new Product(
+        {
+            name:req.body.name,
+            brand:req.body.brand,
+            color:req.body.color,
+            provider:req.body.provider,
+            state:req.body.state,
+            price:req.body.price,
+            sellingPrice:req.body.sellingPrice,
+            quantity:req.body.quantity,
+            category:req.body.category.name,
+            description:req.body.description,
+            image:req.body.image
+        }
+    )
+    product.save().then(data=>{
+        res.json(data);
+    }).catch(err=>res.json(err))
+    
+});
+
+router.put('/', async (req, res) => {
+    Product.findById(req.body._id).then(product=>{
+        console.log(product);
+        product.name=req.body.name || product.name;
+        product.brand=req.body.brand || product.brand;
+        product.color=req.body.color || product.color;
+        product.provider=req.body.provider || product.provider;
+        product.state=req.body.state || product.state;
+        product.price=req.body.price || product.price;
+        product.sellingPrice=req.body.sellingPrice || product.sellingPrice;
+        product.quantity=req.body.quantity || product.quantity;
+        product.category=req.body.category || product.category;
+        product.description=req.body.description || product.description;
+        product.image=req.body.image || product.image;
+
+        product.save().then(data=>{
+            res.json(data);
+        }).catch(err=>res.json(err));
+    })
+});
+
+
+
+router.delete('/:id', async (req, res) => {
+    Product.findByIdAndRemove(req.params.id).then(data=>{
+        res.json(data)
+    }).catch(err=>res.json(err));
+});
+
+router.get('/:id', async (req, res) => {
+    Product.findById(req.params.id).then(data=>{
+        res.json(data)
+    }).catch(err=>res.json(err));
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,79 @@
+const express=require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyparser =require("body-parser");
+multer = require('multer');
+const path = require('path');
+const passport = require('passport');
+
+
+
+
+require('dotenv/config');
+
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 
+}
+
+const cors=require('cors');
+// CROSS ORIGIN RESOURCE SHARING
+app.use(cors(corsOptions));
+app.use(express.static('.'));
+
+const PATH = './uploads';
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, PATH);
+  },
+  filename: (req, file, cb) => {
+    console.log(JSON.stringify(file));
+    cb(null, file.originalname)
+  }
+});
+
+let upload = multer({
+  storage: storage
+});
+
+app.post('/file-upload', upload.single('image'), function (req, res) {
+    if (!req.file) {
+      console.log("No file is available!");
+      return res.send({
+        success: false
+      });
+  
+    } else {
+      console.log('File is available!');
+      return res.send({
+        success: true
+      })
+    }
+  });
+
+
+app.use(bodyparser.json());
+const productsRoute= require('./routes/products');
+const categoriesRoute= require('./routes/categories');
+const colorsRoute= require('./routes/colors');
+const providersRoute= require('./routes/providers');
+const usersRoute= require('./routes/users');
+
+
+
+app.use('/products',productsRoute)
+app.use('/colors',colorsRoute)
+app.use('/categories',categoriesRoute)
+app.use('/providers',providersRoute)
+app.use('/users',usersRoute)
+ 
+mongoose.connect(process.env.DB_CONNECTION,
+{useNewUrlParser: true, useUnifiedTopology: true},
+()=>{
+    console.log('connected to Database');
+});
+
+require('./config/passport')(passport);
+
+app.listen(3000);
